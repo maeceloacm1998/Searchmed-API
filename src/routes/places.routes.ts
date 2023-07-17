11c53375fd7df0a2196a43da7ac7daa646d52ea0
marcelo.cmendes <marcelo.cmendes@americanas.io>
2023-07-17 15:38:12 -0300

    chore: add comment in request

--- src/routes/places.routes.ts
+++ src/routes/places.routes.ts
@@ -2,6 +2,12 @@ import { Router } from "express";
 import { placeAutoCompleteController } from "../controller/place.controller";
 
 function placeAutoCompleteRoute(route: Router) {
+    /**
+     * Essa chamada retorna uam lista de endereços de acordo com o address passado
+     * por parametro.
+     * @params address: String
+     * @returns Array<string>
+     */
     route.get("/place/autocomplete", placeAutoCompleteController)
 }
 
