cdf146c26625653156ab026e7bb4ed52b403da25
marcelo.cmendes <marcelo.cmendes@americanas.io>
2023-07-20 03:44:05 -0300

    feat: config to deploy server

--- app.ts
+++ app.ts
@@ -14,7 +14,29 @@ function middlewares() {
 }
 
 function connectMongDb() {
-    mongoose.connect('mongodb+srv://maeceloacm1998:Vayne123@cluster0.k8a87tc.mongodb.net/');
+    // mongoose.connect('mongodb+srv://maeceloacm1998:Vayne123@cluster0.k8a87tc.mongodb.net/');
+    mongoose
+        .connect(process.env.MONGODB_URI!!, {
+            dbName: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASS
+        })
+        .then(() => {
+            console.log('Mongodb connected....');
+        })
+        .catch(err => console.log(err.message));
+
+    mongoose.connection.on('connected', () => {
+        console.log('Mongoose connected to db...');
+    });
+
+    mongoose.connection.on('error', err => {
+        console.log(err.message);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Mongoose connection is disconnected...');
+    });
 }
 
 middlewares()
