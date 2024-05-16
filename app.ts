a8aa019b2f07d861dd3013f1efc21a625ede32fe
marcelochaves <mcmendes@uolinc.com>
2024-05-16 16:33:27 -0300

    chore: adjust code to get .env variables

--- app.ts
+++ app.ts
@@ -3,45 +3,47 @@ import { invoke } from "./src/routes/index";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
-const app: Application = express()
+const app: Application = express();
 const port = process.env.PORT || 3000;
 
 function middlewares() {
-    app.use(cors())
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(express.json());
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(express.json());
+  dotenv.config();
 }
 
-function connectMongDb() {    
-    mongoose
-        .connect(process.env.MONGODB_URI as string, {
-            dbName: process.env.DB_NAME,
-            user: process.env.DB_USER,
-            pass: process.env.DB_PASS
-        })
-        .then(() => {
-            console.log('Mongodb connected....');
-        })
-        .catch(err => console.log(err.message));
+function connectMongDb() {
+  mongoose
+    .connect(process.env.MONGODB_URI as string, {
+      dbName: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASS,
+    })
+    .then(() => {
+      console.log("Mongodb connected....");
+    })
+    .catch((err) => console.log(err.message));
 
-    mongoose.connection.on('connected', () => {
-        console.log('Mongoose connected to db...');
-    });
+  mongoose.connection.on("connected", () => {
+    console.log("Mongoose connected to db...");
+  });
 
-    mongoose.connection.on('error', err => {
-        console.log(err.message);
-    });
+  mongoose.connection.on("error", (err) => {
+    console.log(err.message);
+  });
 
-    mongoose.connection.on('disconnected', () => {
-        console.log('Mongoose connection is disconnected...');
-    });
+  mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose connection is disconnected...");
+  });
 }
 
-middlewares()
-connectMongDb()
-app.use(invoke())
+middlewares();
+connectMongDb();
+app.use(invoke());
 
 app.listen(port, () => {
-    console.log(`Server iniciado na porta ${port}`)
-})
+  console.log(`Server iniciado na porta ${port}`);
+});
